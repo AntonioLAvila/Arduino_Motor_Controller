9ffdde7323a986857c6b52a01cfca7f923c7cd83
Antonio Avila <antonioavila433@gmail.com>
2023-01-31 14:56:49 -0500

    Add int extractor

--- Arduino_Motor_Controller.ino
+++ Arduino_Motor_Controller.ino
@@ -1,7 +1,21 @@
+//"FL%+4.fFR%+4.fBR%+4.fBL%+4.f\n"
+//"FL+0.2233FR-0.3456BR-0.3454BL+0.9820"
+
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
   
+}
+
+void int_extractor(char* data_array, int* output_values, char delimiter){
+    if(data_array[0] == 0) return;
+    char* split = strtok(data_array, &delimiter);
+    int idx = 0;
+    while(split != NULL){
+        output_values[idx] = atoi(split);
+        split = strtok(NULL, &delimiter);
+        idx++;
+    }
 }
