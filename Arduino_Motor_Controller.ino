edabcf9ea27c87307a6d71cf9ce44fae6a785024
Antonio L Avila <antonioavila433@gmail.com>
2023-01-31 22:14:40 -0500

    stuff

--- Arduino_Motor_Controller.ino
+++ Arduino_Motor_Controller.ino
@@ -1,21 +1,18 @@
-//"FL%+4.fFR%+4.fBR%+4.fBL%+4.f\n"
-//"FL+0.2233FR-0.3456BR-0.3454BL+0.9820"
+#include <string.h>
+
+float powers[4];
+char* intermidiate[4];
+
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  
-}
+  if(Serial.available()){
+    String command = Serial.readStringUntil('\n');
 
-void int_extractor(char* data_array, int* output_values, char delimiter){
-    if(data_array[0] == 0) return;
-    char* split = strtok(data_array, &delimiter);
-    int idx = 0;
-    while(split != NULL){
-        output_values[idx] = atoi(split);
-        split = strtok(NULL, &delimiter);
-        idx++;
-    }
+    char* charCommand[]
+    
+  }
 }
